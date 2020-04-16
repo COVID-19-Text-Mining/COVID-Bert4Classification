4ff3dc4a905f6c0d5d98adc05105159f6ea69b37
Yuxing Fei <yuxingfei24@gmail.com>
2020-04-16 10:49:53 -0700

    fix bug
    predict.py always outputs emtpy result

--- predict.py
+++ predict.py
@@ -51,7 +51,7 @@ class Prediction:
 
     @classmethod
     def predict_one(cls, text: str) -> Dict[str, label_tuple]:
-        if not text.strip():
+        if text.strip():
             return cls.predict_many([text])[0]
         return cls.empty_string_result.copy()
 
@@ -84,7 +84,7 @@ class Prediction:
             prediction = dict()
 
             for i, label in enumerate(output > cls.config.Predict.positive_threshold):
-                prediction[indexes[i]] = cls.label_tuple(bool(label), float(output[i]))
+                prediction[indexes[i]] = label_tuple(bool(label), float(output[i]))
 
             predictions.append(prediction)
 
@@ -92,4 +92,36 @@ class Prediction:
 
 
 if __name__ == "__main__":
-    print(Prediction.predict("Hello, world."))
+    print(Prediction.predict("Since the sudden outbreak of coronavirus"
+                             " disease 2019 (COVID-19), it has rapidly"
+                             " evolved into a momentous global health"
+                             " concern. Due to the lack of constructive"
+                             " information on the pathogenesis of COVID-19"
+                             " and specific treatment, it highlights the"
+                             " importance of early diagnosis and timely"
+                             " treatment. In this study, 11 key blood indices"
+                             " were extracted through random forest algorithm"
+                             " to build the final assistant discrimination tool"
+                             " from 49 clinical available blood test data"
+                             " which were derived by commercial blood test"
+                             " equipments. The method presented robust outcome"
+                             " to accurately identify COVID-19 from a variety"
+                             " of suspected patients with similar CT information"
+                             " or similar symptoms, with accuracy of 0.9795 and"
+                             " 0.9697 for the cross-validation set and test set,"
+                             " respectively. The tool also demonstrated its outstanding"
+                             " performance on an external validation set that was"
+                             " completely independent of the modeling process, with"
+                             " sensitivity, specificity, and overall accuracy of"
+                             " 0.9512, 0.9697, and 0.9595, respectively. Besides,"
+                             " 24 samples from overseas infected patients with"
+                             " COVID-19 were used to make an in-depth clinical"
+                             " assessment with accuracy of 0.9167. After multiple"
+                             " verification, the reliability and repeatability"
+                             " of the tool has been fully evaluated, and it has"
+                             " the potential to develop into an emerging technology"
+                             " to identify COVID-19 and lower the burden of global"
+                             " public health. The proposed tool is well-suited to"
+                             " carry out preliminary assessment of suspected patients"
+                             " and help them to get timely treatment and quarantine "
+                             "suggestion."))
