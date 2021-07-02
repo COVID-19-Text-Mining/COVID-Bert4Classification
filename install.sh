65ed19599bc8df849106849625ec487007fc1ddf
Yuxing Fei <36567193+idocx@users.noreply.github.com>
2021-07-02 17:00:17 +0800

    feature(results): add scibert results and introduce a summary in README.md

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 pip install -r requirements.txt
-mkdir checkpoints/bst_model -p
-#wget https://old.yuxingfei.com/src/litcovid.zip
-#unzip litcovid.zip
+wget https://old.yuxingfei.com/src/litcovid.zip
+unzip litcovid.zip
